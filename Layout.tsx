ff3525d9767f5f3a3bcfc2f6678884c2851673b2
Liyana <liyana@wego.com>
2020-04-25 18:05:29 +0800

    Final changes

--- Layout.tsx
+++ Layout.tsx
@@ -12,10 +12,10 @@ const Layout = (): JSX.Element => {
       <div className="right-container">
         <ScrollThenFixed>
           <div style={{ height: "50px", backgroundColor: "wheat" }}>
-            This is a left container, it's scroll height is 50px
+            This is a left container, it's scroll height is 1050px
           </div>
 
-          <div style={{ height: "1900px", backgroundColor: "aliceblue" }}>
+          <div style={{ height: "900px", backgroundColor: "aliceblue" }}>
             Item 2
           </div>
 
